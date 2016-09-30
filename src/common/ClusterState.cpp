27973b1b04e2ab50b7b53245dd45877db0e6580c
kaiserj <kaiserj@uni-mainz.de>
2016-09-30 22:04:38 +0200

    Fix crash

--- src/common/ClusterState.cpp
+++ src/common/ClusterState.cpp
@@ -6,117 +6,118 @@
 
 namespace common {
 
-bool MemoryClusterState::getOstState(const std::string &id, OSTWorkload *state) {
-    std::lock_guard<std::mutex> lck(state_mut);
+    bool MemoryClusterState::getOstState(const std::string &id, OSTWorkload *state) {
+        std::lock_guard<std::mutex> lck(state_mut);
 
-    auto it = this->ost_state_map.find(id);
-    if (it == this->ost_state_map.end()) {
-        return false;
-    }
-    *state = it->second;
-    return true;
-}
-
-std::vector<std::string> *MemoryClusterState::GetOSTList() {
-    std::lock_guard<std::mutex> lck(state_mut);
-    std::vector<std::string> *names = new std::vector<std::string>();
-    names->reserve(this->ost_state_map.size());
-
-    for (auto i = this->ost_state_map.begin(); i != this->ost_state_map.end(); i++) {
-        names->push_back(i->second.name);
+        auto it = this->ost_state_map.find(id);
+        if (it == this->ost_state_map.end()) {
+            return false;
+        }
+        *state = it->second;
+        return true;
     }
 
-    return names;
-}
+    std::vector<std::string> *MemoryClusterState::GetOSTList() {
+        std::lock_guard<std::mutex> lck(state_mut);
+        std::vector<std::string> *names = new std::vector<std::string>();
+        names->reserve(this->ost_state_map.size());
 
+        for (auto i = this->ost_state_map.begin(); i != this->ost_state_map.end(); i++) {
+            names->push_back(i->second.name);
+        }
 
-
-bool MemoryClusterState::Init() {
-
-    if (!Update()) {    // perform an initial update first
-        return false;
+        return names;
     }
 
-    if (not update_thread_started) {
-        update_thread = std::thread(&MemoryClusterState::updateRepeatedly, this);
-        update_thread_started = true;
-    } else { // something's wrong. Was it initialized before?
-        return false;
+
+    bool MemoryClusterState::Init() {
+
+        if (!Update()) {    // perform an initial update first
+            return false;
+        }
+
+        if (not update_thread_started) {
+            update_thread = std::thread(&MemoryClusterState::updateRepeatedly, this);
+            update_thread_started = true;
+        } else { // something's wrong. Was it initialized before?
+            return false;
+        }
+
+        return true;
     }
 
-    return true;
-}
+    bool MemoryClusterState::TearDown() {
+        std::unique_lock<std::mutex> lck(state_mut);
 
-bool MemoryClusterState::TearDown() {
-    std::unique_lock<std::mutex> lck(state_mut);
-
-    update_thread_exit_flag = true;
-    while (update_thread_is_active) {
-        update_thread_finish_cv.wait(lck);
-    }
-    lck.unlock();
-    update_thread.join();
-    return true;
-}
-
-
-
-void MemoryClusterState::updateRepeatedly() {
-
-    std::unique_lock<std::mutex> lck(state_mut);
-    if (update_thread_exit_flag) {
+        update_thread_exit_flag = true;
+        while (update_thread_is_active) {
+            update_thread_finish_cv.wait(lck);
+        }
         lck.unlock();
-        return;
-    }
-    update_thread_is_active = true;
-    update_thread_finish_cv.notify_all();
-    lck.unlock();
 
-    while(!update_thread_exit_flag) {
+        if (update_thread.joinable()){
+            update_thread.join();
+        }
 
-        if (!Update()) {
-           //TODO: add error logging here, but don't exit the thread as it might be an temporary error
-        };
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        return true;
     }
 
-    lck.lock();
-    update_thread_is_active = false;
-    update_thread_finish_cv.notify_all();
-    lck.unlock();
-}
 
-bool MemoryClusterState::Update() {
-    std::shared_ptr<std::vector<Lustre::getOstsResults>> r (new std::vector<Lustre::getOstsResults>());
+    void MemoryClusterState::updateRepeatedly() {
 
-    if (!lustre->GetOstList("", r)) {
-        return false;
+        std::unique_lock<std::mutex> lck(state_mut);
+        if (update_thread_exit_flag) {
+            lck.unlock();
+            return;
+        }
+        update_thread_is_active = true;
+        update_thread_finish_cv.notify_all();
+        lck.unlock();
+
+        while (!update_thread_exit_flag) {
+
+            if (!Update()) {
+                //TODO: add error logging here, but don't exit the thread as it might be an temporary error
+            };
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+
+        lck.lock();
+        update_thread_is_active = false;
+        update_thread_finish_cv.notify_all();
+        lck.unlock();
     }
 
-    std::unique_lock<std::mutex> lck(state_mut);
-    for (auto &it : *r) {
-        // TODO: obviously, the following performance values are wild guesses. Replace with correct ones! Implement some heuristic to derive some initial max value?
-        //this->ost_state_map[it.number] = {it.uuid, 0, 0};
-        this->ost_state_map[it.number] = {it.uuid, 0, default_rpc_rate_};
+    bool MemoryClusterState::Update() {
+        std::shared_ptr<std::vector<Lustre::getOstsResults>> r(new std::vector<Lustre::getOstsResults>());
+
+        if (!lustre->GetOstList("", r)) {
+            return false;
+        }
+
+        std::unique_lock<std::mutex> lck(state_mut);
+        for (auto &it : *r) {
+            // TODO: obviously, the following performance values are wild guesses. Replace with correct ones! Implement some heuristic to derive some initial max value?
+            //this->ost_state_map[it.number] = {it.uuid, 0, 0};
+            this->ost_state_map[it.number] = {it.uuid, 0, default_rpc_rate_};
+        }
+
+        lck.unlock();
+
+        return true;
     }
 
-    lck.unlock();
+    void MemoryClusterState::UpdateNode(const std::string &name, const OSTWorkload &node_state) {
+        ost_state_map[name] = node_state;
+    }
 
-    return true;
-}
+    MemoryClusterState::MemoryClusterState(std::shared_ptr<common::Lustre> l) : update_thread_started(false),
+                                                                                update_thread_is_active(false),
+                                                                                update_thread_exit_flag(false),
+                                                                                lustre(l) {}
 
-void MemoryClusterState::UpdateNode(const std::string &name, const OSTWorkload &node_state) {
-    ost_state_map[name] = node_state;
-}
-
-MemoryClusterState::MemoryClusterState(std::shared_ptr<common::Lustre> l) :  update_thread_started(false),
-                                                                             update_thread_is_active(false),
-                                                                             update_thread_exit_flag(false) {
-    lustre = l;
-}
-
-MemoryClusterState::MemoryClusterState() : update_thread_started(false),
-    update_thread_is_active(false),
-    update_thread_exit_flag(false) {}
+    MemoryClusterState::MemoryClusterState() : update_thread_started(false),
+                                               update_thread_is_active(false),
+                                               update_thread_exit_flag(false) {}
 
 }
