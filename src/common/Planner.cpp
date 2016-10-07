2cabf6299efa1fa636d2c550b8a9032bd3605b8b
kaiserj <kaiserj@uni-mainz.de>
2016-10-07 13:54:41 +0200

    Set the end date to max if requested duration is 0

--- src/common/Planner.cpp
+++ src/common/Planner.cpp
@@ -72,7 +72,8 @@ bool Planner::ServeJobSubmission(rpc::Message &msg) {
     }
 
     auto tstart = std::chrono::system_clock::now();
-    auto tend = tstart + std::chrono::seconds(request.durationsec());;
+    auto tend = (request.durationsec() == 0) ? std::chrono::time_point::max() : tstart + std::chrono::seconds(
+            request.durationsec());
     auto job = BuildJob(request, tstart, tend, osts_set);
     bool success = scheduler->ScheduleJob(*job);
     msg.mutable_reply()->set_rc((success) ? 0 : -1);
