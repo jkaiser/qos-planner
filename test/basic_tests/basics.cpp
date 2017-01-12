88fa2cb79f28059b201d795c3f469be5fb1be3c7
jkaiser <kaiserj@uni-mainz.de>
2017-01-12 12:23:14 +0100

    Initial commit

---
+++ test/basic_tests/basics.cpp
@@ -0,0 +1,13 @@
+//
+// Created by jkaiser on 01.03.16.
+//
+
+#include "gtest/gtest.h"
+
+TEST(basic_check, test_eq) {
+    EXPECT_EQ(1, 0);
+};
+
+TEST(basic_check, test_neq) {
+    EXPECT_NE(1, 0);
+};
