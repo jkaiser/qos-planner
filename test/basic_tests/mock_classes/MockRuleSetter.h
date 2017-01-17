2102206cf9ef9327e8722b4a288f4d28571caeb7
kaiserj <kaiserj@uni-mainz.de>
2017-01-17 10:39:56 +0100

    Add missing GPL headers

--- test/basic_tests/mock_classes/MockRuleSetter.h
+++ test/basic_tests/mock_classes/MockRuleSetter.h
@@ -1,6 +1,26 @@
-//
-// Created by Jürgen Kaiser on 02.11.16.
-//
+/*
+ * Copyright (c) 2017 Jürgen Kaiser
+ */
+/*
+ * GPL HEADER START
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details (a copy is included
+ * in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; If not, see <http://www.gnu.org/licenses/>.
+ *
+ * GPL HEADER END
+ */
 
 #ifndef QOS_PLANNER_MOCKRULESETTER_H
 #define QOS_PLANNER_MOCKRULESETTER_H
